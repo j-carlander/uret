68cc65be31a90b23380279ab479df808120616ed
j-carlander <johannes.carlander@gmail.com>
2025-01-07 21:57:29 +0100

    add backend route for login with jsonwebtoken

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import { runQuery } from "../db.js";
+import jwtUtil from "../utils/jwtUtil.js";
 
 export const router = express.Router();
 
@@ -19,4 +20,27 @@ router.post("/user", async (req, res) => {
   } else {
     res.status(400).json({ message: "Kontot skapades inte" });
   }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(403).json({ message: "Saknar email eller lösenord!" });
+
+  const user = await runQuery("SELECT * FROM users WHERE email = ?", [email]);
+
+  if (user.length === 0)
+    return res.status(403).json({ message: "Fel email eller lösenord!" });
+
+  const storedHash = user[0].password;
+
+  const passwordMatch = await bcrypt.compare(password, storedHash);
+
+  if (!passwordMatch)
+    return res.status(403).json({ message: "Fel email eller lösenord!" });
+
+  const token = jwtUtil.createToken({ name: user[0].name });
+
+  res.status(200).json({ message: "Inloggad", token });
 });
