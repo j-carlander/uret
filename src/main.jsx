a6a37a98307d3e15bce49c3dc7c4583eef447dfa
j-carlander <johannes.carlander@gmail.com>
2025-01-14 23:53:01 +0100

    fix: protect routes by checking for user cookie

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Login } from './pages/login.jsx';
-import { Register } from './pages/Register.jsx';
-import { Home } from './pages/Home.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
+import { Login } from "./pages/login.jsx";
+import { Register } from "./pages/Register.jsx";
+import { Home } from "./pages/Home.jsx";
+import { getCurrentUser } from "./utils/currentUser.js";
 
-export const pages = [
-  { path: "/", element: <Home />},
-  { path: "/logga-in", element: <Login /> },
-  { path: "/registrera", element: <Register /> },
-];
-
-
+export const pages = [{ path: "/", element: <Home /> }];
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: pages,
+    loader: () => {
+      const currentUser = getCurrentUser();
+      if (!currentUser) {
+        return redirect("/logga-in");
+      }
+      return null;
+    },
   },
+  {
+    path: "/logga-in",
+    element: <Login />,
+    loader: () => {
+      const currentUser = getCurrentUser();
+      if (currentUser) {
+        return redirect("/");
+      }
+      return null;
+    },
+  },
+  { path: "/registrera", element: <Register /> },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
